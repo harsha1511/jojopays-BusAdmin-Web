8de616a1a2861dafcda55184ff2be06bf9f29403
harsha1511 <hv@jojopays.com>
2022-11-11 13:29:16 +0530

    Source Control

--- src/pages/Wallet/components/TopupWallet.tsx
+++ src/pages/Wallet/components/TopupWallet.tsx
@@ -6,7 +6,7 @@ export const TopupWallet = () => {
     <div>
         <div className='flex flex-col items-center justify-center h-full'>
             <div className='flex flex-col items-center w-[900px] h-60 mt-10 bg-quaternary rounded-sm rounded-br-xl rounded-tl-[30px]'> 
-                <p className='mt-4'>Top-up Wallet</p>
+                <p className='mt-4'>Topup Wallet</p>
                 <input
                 placeholder='Enter Amount'
                 className='w-[80%] mt-6 h-[45px] bg-primary px-4 py-1 rounded-md border-2 border-blueText'
